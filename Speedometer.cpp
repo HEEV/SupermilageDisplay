7ca65c8650d6842e218763f2df57d59e660a5453
Andrew Huffman <ahuffman@cedarville.edu>
2021-11-06 11:49:25 -0400

    Tweaked Speedometer class definition to better reflect the problem. Needs testing. Constants.h for physical constants.

--- Speedometer.cpp
+++ Speedometer.cpp
@@ -1,13 +1,28 @@
 #include "Speedometer.h"
 
-Speedometer::Speedometer(juce::Point<int> center) {
+#include "Constants.h"
+#include <stdexcept>
+
+Speedometer::Speedometer(juce::Point<int> center, float min, float max) {
 	this->center = center;
+	setDataRange(min, max);
+
+Speedometer::Speedometer(juce::Point<int> center):
+       Speedometer(center, 0, 100)	{
 }
 
+/**
+ * Destructor.
+ */
 Speedometer::~Speedometer() {
 
 }
 
+/**
+ * Draws the speedometer within the graphics context.
+ *
+ * @param g The JUCE graphics context.
+ */
 void Speedometer::draw(juce::Graphics& g) {
 	juce::Rectangle<float> house(center.x - 10, center.y - 10, 20, 20);
 	g.setColour(juce::Colours::royalblue);
@@ -22,15 +37,57 @@ void Speedometer::draw(juce::Graphics& g) {
 	g.fillPath(path);
 }
 
+/**
+ * Sets the rotation of the speedometer pin, from a range of
+ * 0 radians to 2pi radians.
+ *
+ * @param newRotation the radian value.
+ *
+ **/
 void Speedometer::setRotation(float newRotation) {
 	this->rotation = newRotation;
 }
 
+/**
+ * Sets the range of data points in the speedometer.
+ *
+ * @param min The minimum speedometer value.
+ * @param max The maximum speedometer value.
+ */
 void Speedometer::setDataRange(float min, float max) {
 	this->dataMin = min;
 	this->dataMax = max;
 }
 
-void Speedometer::updateDataValue(float value) {
-	setRotation(3.1415 * (dataMax + value) / dataMax);
+/**
+ * Sets the speedometer value.
+ *
+ * @param The speedometer value.
+ * @throws std::out_of_range
+ */
+void Speedometer::setData(float value) {
+	// setRotation(3.1415 * (dataMax + value) / dataMax);
+	
+	if (value < dataMin || value > dataMax) {
+		throw std::out_of_range("value must be within the provided data range.");
+	}
+
+	data = value;
+
+	// Range the data around 0
+	float weight = dataMax - dataMin;
+	value = value - dataMin;
+
+	// Set the rotation
+	setRotation(PI * (weight + value) / weight);
+	
+}
+
+/**
+ * Gets the data that is displaying on the speedometer.
+ *
+ * @return The speedometer data.
+ */
+float Speedometer::getData() const {
+	return data;
 }
--- Speedometer.h
+++ Speedometer.h
@@ -6,17 +6,32 @@ class Speedometer
 {
 
 public:
+	Speedometer(juce::Point<int> center, float min, float max);
 	Speedometer(juce::Point<int> center);
 	~Speedometer();
 
+	// # JUCE METHODS
+	
 	void draw(juce::Graphics& g);
-	void setRotation(float newRotation);
+
+	// # SPEEDOMETER INTERFACING
+	
 	void setDataRange(float min, float max);
-	void updateDataValue(float value);
+	void setData(float value); // throws std::out_of_range
+	float getData() const;
 
 private:
+	void setRotation(float newRotation);
+
+	// # DISPLAY DATA
+
 	juce::Point<int> center;
 	float rotation;
+
+	// # SPEEDOMETER DATA
+
 	float dataMin;
 	float dataMax;
+	float data;
+
 };
