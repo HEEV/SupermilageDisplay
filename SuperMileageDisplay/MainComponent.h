1aec547c8213eebe2aaea4ce61c7526ebe95c5f8
Jaynard2 <joshua.grieve19@gmail.com>
2022-02-12 09:21:29 -0500

    Created a CMake build system

---
+++ SuperMileageDisplay/MainComponent.h
@@ -0,0 +1,39 @@
+#pragma once
+
+#include <JuceHeader.h>
+
+//==============================================================================
+/*
+    This component lives inside our window, and this is where you should put all
+    your controls and content.
+*/
+class MainComponent  : public juce::AnimatedAppComponent
+{
+public:
+    //==============================================================================
+    MainComponent();
+    ~MainComponent() override;
+
+    //==============================================================================
+    void paint (juce::Graphics&) override;
+    void resized() override;
+    void update() override;
+
+private:
+    //==============================================================================
+    // Your private member variables go here...
+    juce::String currentSizeAsString;
+
+    juce::Point<int> speedP1;
+    juce::Point<int> speedP2;
+    juce::Point<int> speedP3;
+    juce::Point<int> speedP4;
+    juce::Point<int> speedP5;
+    juce::Point<int> speedP6;
+    juce::Point<int> speedP7;
+    juce::Point<int> speedP8;
+
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(MainComponent)
+
+    float rotate = 0;
+};
