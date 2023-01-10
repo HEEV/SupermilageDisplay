5a825e58218e8eb209b7592b861479d7be0fc93a
Joshua <joshua.grieve19@gmail.com>
2023-01-10 15:32:01 -0500

    Connect to fast-dds server

--- src/Communications/CommunicationManager.cpp
+++ src/Communications/CommunicationManager.cpp
@@ -4,25 +4,40 @@
 #include "Communication/PacketTypes/header.h"
 
 using namespace eprosima::fastdds::dds;
+using namespace eprosima::fastrtps::rtps;
 
 CommunicationManager::CommunicationManager()
 {
-    DomainParticipantQos qos;
-    eprosima::fastrtps::rtps::RemoteServerAttributes server;
-    eprosima::fastrtps::rtps::Locator_t loc;
-    qos.wire_protocol().builtin.discovery_config.discoveryProtocol = eprosima::fastrtps::rtps::DiscoveryProtocol_t::CLIENT;
-    std::istringstream("44.53.00.5f.45.50.52.4f.53.49.4d.41") >> qos.wire_protocol().prefix;
-    eprosima::fastrtps::rtps::IPLocator::setIPv4(loc, "127.0.0.1");
-    loc.port = 25565;
-    server.metatrafficUnicastLocatorList.push_back(loc);
-    server.ReadguidPrefix("11.11.11.11.11.11.11.11.11.11.11.11");
-    qos.wire_protocol().builtin.discovery_config.m_DiscoveryServers.push_back(server);
-    qos.wire_protocol().builtin.discovery_config.discoveryServer_client_syncperiod = eprosima::fastrtps::Duration_t(0, 250000000);
+    // Get default participant QoS
+    DomainParticipantQos client_qos = PARTICIPANT_QOS_DEFAULT;
 
-    _particpant = 
-        DomainParticipantFactory::get_instance()->create_participant(0, qos);
+    // Set participant as CLIENT
+    client_qos.wire_protocol().builtin.discovery_config.discoveryProtocol =
+            DiscoveryProtocol_t::CLIENT;
+
+    // Set SERVER's GUID prefix
+    RemoteServerAttributes remote_server_att;
+    remote_server_att.ReadguidPrefix("44.53.00.5f.45.50.52.4f.53.49.4d.41");
+
+    // Set SERVER's listening locator for PDP
+    Locator_t locator;
+    IPLocator::setIPv4(locator, 172, 18, 0, 2);
+    locator.port = 25565;
+    remote_server_att.metatrafficUnicastLocatorList.push_back(locator);
+
+    // Add remote SERVER to CLIENT's list of SERVERs
+    client_qos.wire_protocol().builtin.discovery_config.m_DiscoveryServers.push_back(remote_server_att);
+
+    // Set ping period to 250 ms
+    client_qos.wire_protocol().builtin.discovery_config.discoveryServer_client_syncperiod =
+            eprosima::fastrtps::Duration_t(0, 250000000);
+
+    // Create CLIENT
+    _particpant =
+            DomainParticipantFactory::get_instance()->create_participant(0, client_qos);
 
     _publisher = _particpant->create_publisher(PUBLISHER_QOS_DEFAULT);
+
 }
 
 CommunicationManager::~CommunicationManager()
