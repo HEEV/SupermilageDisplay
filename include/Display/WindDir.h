0c9d57ec7df6a9944e6b49f5fcbe904615bf1594
Seth Burkey <sburkey@cedarville.edu>
2023-10-14 10:43:42 -0400

    created the WindDir.h and WindDir.cpp files

---
+++ include/Display/WindDir.h
@@ -0,0 +1,25 @@
+#pragma once
+#include <JuceHeader.h>
+
+class WindDir : public AnimatedAppComponent
+{
+    public:
+        WindDir(std::string name, float arrowWidth, float arrowLength);
+        ~WindDir();
+
+        //JUCE methods
+
+        void paint(juce::Graphics& g) override;
+	    void resized() override { /*do nothing*/ }
+	    void update() override;
+
+        void setData(float data);
+        void setName(std::string name);
+    
+    private:
+        float _data;
+        float _arrowWidth;
+        float _arrowLength;
+        std::string _name;
+        juce::Colour _color;
+};
---
+++ src/Display/WindDir.cpp
@@ -0,0 +1,6 @@
+#include "Display/WindDir.h"
+
+WindDir::WindDir(std::string name, float arrowWidth, float arrowLength) : _name(name), _arrowWidth(arrowWidth), _arrowLength(arrowLength)
+{
+    
+}
